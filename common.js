8b63013f643c24c50a4289d1d0ff6ee440e57c29
mint <catbox@outlook.jp>
2021-07-14 22:54:09 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,9 @@
-
+props: {
+  title: "潜水艦用ツール",
+  //likes: Number,
+  //isPublished: false,
+  //commentIds: Array,
+  //author: Object,
+  //callback: Function,
+  //contactsPromise: Promise // or any other constructor
+}
